d0aae080ba73a0c8c1d4d4ad65f2bfe91b2f1223
NITISH <nitish.54868@gmail.com>
2025-08-19 14:19:45 +0530

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,22 @@
+package practictical;
+
+public class Encapsulation {
+    
+    public static void main(String[] args) {
+        Programmer p = new Programmer();
+        p.setName("Geek"); 
+        System.out.println("Name=> " + p.getName());
+    }
+}
+
+class Programmer {
+    
+    private String name;
+
+    
+    public String getName() { return name; }
+    
+    // Setter method is used to set or modify the data
+    public void setName(String name) { this.name = name; }
+}
+
